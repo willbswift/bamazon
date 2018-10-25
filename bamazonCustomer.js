48dc0f2d4f1693eb42c8a5d815f981f5c8c2e0ef
William B. Swift <willbswift@hotmail.com>
2018-10-25 18:31:29 -0500

    trying to commit!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+let confirm = require('inquirer-confirm');
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -18,8 +19,9 @@ var connection = mysql.createConnection({
 
 // connect to the mysql server and sql database
 connection.connect(function(err) {
-  console.log("connected as id " + connection.threadId);
+  //console.log("connected as id " + connection.threadId);
   if (err) throw err;
+  console.log("Welcome to Quarks Black Market Sales!");
   // run the start function after the connection is made to prompt the user
   start();
 });
@@ -27,23 +29,24 @@ connection.connect(function(err) {
 // function which displays availvible products and prompts the user type id number of product they wish to buy.  
 function start() {
   queryAllProducts();
-  buyStuff();
 };
 
 function queryAllProducts() {
   connection.query("SELECT * FROM products", function(err, results) {
-    console.log(results);
+    //console.log(results);
+    console.log("Here is what we have for sale today...");
+    console.log("-----------------------------------");
+    //console.log("ID#   | Product Name | Type | Price(Latinum) | Stock");
     for (let i = 0; i < results.length; i++) {
-      console.log("Welcome to Quarks Black Market Sales.  Here is what we have for sale today...");
-      console.log("Item ID# | Product Name | Type | Price(Latinum) | Stock");
-      console.log(results[i].item_id + " | " + results[i].product_name + " | " + results[i].department_name + " | $" + results[i].price + " | " + results[i].stock_quantity);
+      console.log("ID# " + results[i].item_id + " | " + results[i].product_name + " | " + results[i].department_name + " | Price $" + results[i].price + " | Stock " + results[i].stock_quantity);
     }
     console.log("-----------------------------------");
+    buyStuff(results);
   });
 }
 
 // function to handle buying stuff
-function buyStuff() {
+function buyStuff(results) {
   // The app should then prompt users with two messages.
   inquirer
     // prompt for info about the item being purchased
@@ -52,7 +55,7 @@ function buyStuff() {
         {
           name: "itemID",
           type: "input",
-          message: "Type the ID number of the item you wish to purchase",
+          message: "Type the ID number of the item you wish to purchase.",
           validate: function(value) {
               if (isNaN(value) === false) {
               return true;
@@ -64,56 +67,74 @@ function buyStuff() {
         {
           name: "quantity",
           type: "input",
-          message: "How many do you wish to purchase?"
+          message: "How many do you wish to purchase?",
+          validate: function(value) {
+            if (isNaN(value) === false) {
+            return true;
+            }
+            return false;
+        }
         }
       ])
     .then(function(answer) {
       // get the information of the chosen item
+      //console.log(answer.itemID);
+      //console.log(answer.quantity);
+      //console.log(results);
       let chosenItem;
-        for (let i = 0; i < results.length; i++) {
-          if (results[i].item_id === answer.itemID) {
-            chosenItem = results[i];
-          }
-        }  
-        // determine if there is enough of that item
-        if (chosenItem.stock_quantity <= parseInt(answer.quantity)) {
-          newQuantity = chosenItem.stock_quantity - parseInt(answer.quantity);
-          // the quantity was high enough, so update db, let the user know, 
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              // This means updating the SQL database to reflect the remaining quantity.
-              {
-                stock_quantity: newQuantity
-              },
-              {
-                item_id: chosenItem.id
-              }
-            ],
-            function(error) {
-              if (error) throw err;
-              // fulfill the customer's order.
-              console.log("Transaction completed!");
-
-// Once the update goes through, show the customer the total cost of their purchase.
-
-
-              // start over
-              start();
+      //console.log(results[3].item_id);
+      for (let i = 0; i < results.length; i++) {
+        if (results[i].item_id === parseInt(answer.itemID)) {
+          chosenItem = results[i];
+        }
+      }  
+      // determine if there is enough of that item
+      if (parseInt(answer.quantity) <= chosenItem.stock_quantity) {
+        newQuantity = chosenItem.stock_quantity - parseInt(answer.quantity);
+        // the quantity was high enough, so update db, let the user know, 
+        //console.log(chosenItem.item_id)
+        connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            // This means updating the SQL database to reflect the remaining quantity.
+            {
+              stock_quantity: newQuantity
+            },
+            {
+              item_id: chosenItem.item_id
             }
-          );
-        }
-        else {
-          // If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-          console.log("I told you I don't have that many! Try again...");
-          // start over
-          start();
-        }
+          ],
+          function(error) {
+            if (error) throw err;
+            console.log(newQuantity);
+            // fulfill the customer's order.
+            console.log("Transaction completed!");
+            // Once the update goes through, show the customer the total cost of their purchase.
+            let totalCost = parseInt(answer.quantity) * chosenItem.price
+            console.log("Total Cost is $" + totalCost);
+            console.log("I'm sure I must have something else you would like!");
+              // ask if they wish to continue
+              let confirm = require('inquirer-confirm');
+              confirm('Would you like to buy something else?')
+                .then(function confirmed() 
+                {
+                  // start over
+                  start();
+                }, function cancelled() 
+                {
+                  console.log('sorry to hear that');
+                  let code = 0;
+                  process.exit(code);
+                });
+          }
+        );
+      }
+      else {
+        // If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+        console.log("I told you I don't have that many! Try again...");
+        // start over
+        buyStuff(results);
+      }
 
-
-        
-
-
-
-
-
+    });
+};
